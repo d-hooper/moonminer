056cd4c65b8d8e8f47d8077fe1de378b360206be
d-hooper <dallinhoop3@gmail.com>
2025-02-21 11:30:12 -0700

    created mine function that adjusts a global variable when clicking the moon

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 //SECTION - STATE
 
+let cheese = 0
+
+let cheeseIncrease = 1
+
 //!SECTION
 
 //SECTION - LOGIC
+
+function mine() {
+  cheese += cheeseIncrease
+  console.log('Cheese increase:', cheeseIncrease, 'Cheese Total:', cheese)
+}
 
 //!SECTION
 
